5849dce6b8ac9c8d65fa74034a3aed921e9728cb
Cristina <Cristina@Lenovo-z510>
2015-04-19 13:50:20 +0200

    AgenteSubastador: Launch one behaviour for each book.

--- subastaVeloz/src/subastador/AgenteSubastador.java
+++ subastaVeloz/src/subastador/AgenteSubastador.java
@@ -2,15 +2,27 @@ package subastador;
 
 import jade.core.Agent;
 
+import java.util.HashMap;
+
 @SuppressWarnings("serial")
 public class AgenteSubastador extends Agent{
 	
-	private Float price = (float) 60.0;
 	private Float incremento = (float) 5.0;
+	private HashMap <String, Float> books;
+	
+	
+	
 	
 	protected void setup() {
+		books.put("Don Quijote", (float)20.0);
+		books.put("Eragon", (float)15.0);
+		books.put("El nombre de la rosa", (float)12.0);
+		
 		System.out.println("Hola! " + getAID().getName() + " está listo");
-		addBehaviour(new SubastadorBehaviour(this, 10000, price, incremento));
+		for (String book : books.keySet()){
+			addBehaviour(new SubastadorBehaviour(this, 10000, book, books.get(book), incremento));
+		}
+		
 	}
 	
 	protected void takeDown() {
