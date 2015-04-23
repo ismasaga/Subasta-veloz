43c5d37e7a7d49296507a3a79ff0742520a7dc4b
Cristina <Cristina@Lenovo-z510>
2015-04-23 16:11:32 +0200

    PujadorGUI: Remove unused imports.

--- subastaVeloz/src/pujador/PujadorGUI.java
+++ subastaVeloz/src/pujador/PujadorGUI.java
@@ -1,16 +1,18 @@
 package pujador;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
 
 @SuppressWarnings("serial")
 public class PujadorGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,12 +34,17 @@ public class PujadorGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public PujadorGUI() {
+		setTitle("Agente Pujador");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		table.setBounds(33, 206, 366, -151);
+		contentPane.add(table);
 	}
-
 }
