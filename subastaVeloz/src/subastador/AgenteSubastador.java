7e04540d6503742a70ae12c2bcf73503b96f3cbc
Cristina <Cristina@Lenovo-z510>
2015-04-23 17:29:36 +0200

    AgenteSubastador: Change books type from ArrayList to HashMap.

--- subastaVeloz/src/subastador/AgenteSubastador.java
+++ subastaVeloz/src/subastador/AgenteSubastador.java
@@ -2,23 +2,24 @@ package subastador;
 
 import jade.core.Agent;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+
 import book.Book;
 
 @SuppressWarnings("serial")
 public class AgenteSubastador extends Agent{
 	
-	private ArrayList <Book> books;
+	private HashMap <Book, Float> books;
 	private Float increase = (float)5.0;
 	protected void setup() {
-		books = new ArrayList<>();
-		books.add(new Book ("Don Quijote", (float)10.0));
-		books.add(new Book ("El perfume", (float)12.0));
-		books.add(new Book ("El nombre de la rosa", (float)15.0));
+		books = new HashMap<>();
+		books.put(new Book ("Don Quijote", (float)10.0), increase);
+		books.put(new Book ("El perfume", (float)12.0), increase);
+		books.put(new Book ("El nombre de la rosa", (float)15.0), increase);
 		
 		System.out.println("Hola! " + getAID().getName() + " está listo");
-		for (Book book : books){
-			addBehaviour(new SubastadorBehaviour(this, 10000, book, increase));
+		for (Book book : books.keySet()){
+			addBehaviour(new SubastadorBehaviour(this, 10000, book, books.get(book)));
 		}
 	}
 	
