b23f3872ea5c2271eb6552313c286cc7fd33a80b
Cristina <Cristina@Lenovo-z510>
2015-04-30 17:25:27 +0200

    Call AgenteSubastadorMethods in order to update table

--- subastaVeloz/src/subastador/SubastadorBehaviour.java
+++ subastaVeloz/src/subastador/SubastadorBehaviour.java
@@ -1,7 +1,6 @@
 package subastador;
 
 import jade.core.AID;
-import book.Book;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
@@ -13,25 +12,25 @@ import jade.lang.acl.MessageTemplate;
 
 import java.util.ArrayList;
 
-public class SubastadorBehaviour extends TickerBehaviour {
-	
-	private String book;
-	private Float price;
-	private Float increase;
+import book.Book;
 
-	public SubastadorBehaviour(Agent a, int period, Book book, Float increase) {
+public class SubastadorBehaviour extends TickerBehaviour {
+
+	private Book book;
+	private static final long serialVersionUID = 1L;
+	private ArrayList<AID> buyerAgents;
+	private AID winner;
+	private AgenteSubastador subastador;
+
+	public SubastadorBehaviour(Agent a, int period, Book book) {
 		super(a, period);
-		this.book = book.getTitle();
-		this.price = book.getPrice();
-		this.increase = increase;
+		this.book = book;
+		this.subastador = (AgenteSubastador) a;
 	}
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	private ArrayList <AID> buyerAgents;
-	private AID winner;
 
 	@Override
 	protected void onTick() {
@@ -42,113 +41,132 @@ public class SubastadorBehaviour extends TickerBehaviour {
 		boolean haveWinner = false;
 		try {
 			DFAgentDescription[] result = DFService.search(myAgent, template);
-			System.out.println("BUSQUEDA REALIZADA"); 
+			System.out.println("BUSQUEDA REALIZADA");
 			buyerAgents = new ArrayList<>();
 			for (DFAgentDescription ad : result) {
 				buyerAgents.add(ad.getName());
 			}
-			
-			if (!buyerAgents.isEmpty()){
+
+			if (!buyerAgents.isEmpty()) {
 				ACLMessage message = new ACLMessage(ACLMessage.CFP);
 				message.setSender(myAgent.getAID());
-				message.setContent(String.valueOf(price));
+				message.setContent(String.valueOf(book.getPrice()));
 				for (AID buyer : buyerAgents) {
 					message.addReceiver(buyer);
 				}
-				message.setConversationId(book);
+				message.setConversationId(book.getTitle());
 				message.setReplyWith("interested" + System.currentTimeMillis());
 				myAgent.send(message);
-				MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId(book), MessageTemplate.MatchInReplyTo(message.getReplyWith()));
-				
-				int replyCount = 0; 
+				MessageTemplate mt = MessageTemplate.and(
+						MessageTemplate.MatchConversationId(book.getTitle()),
+						MessageTemplate.MatchInReplyTo(message.getReplyWith()));
+
+				int replyCount = 0;
 				int interestedCount = 0;
-				
+
 				do {
 					ACLMessage reply = myAgent.receive(mt);
 					if (reply != null) {
 						String content = reply.getContent();
-						if (content.equals("interested")){
+						if (content.equals("interested")) {
 							if (!haveWinner) {
 								haveWinner = true;
 								winner = reply.getSender();
 							}
-							System.out.println(reply.getSender().getName() + "acepta pujar por " + message.getConversationId() + " por " + price + " euros");
-							interestedCount ++;
+							System.out.println(reply.getSender().getName()
+									+ "acepta pujar por "
+									+ message.getConversationId() + " por "
+									+ book.getPrice() + " euros");
+							interestedCount++;
 						}
-						replyCount ++;
-					}
-					else {
+						replyCount++;
+					} else {
 						block();
 					}
 				} while (replyCount < buyerAgents.size());
-				
-				
+
 				message = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
 				message.setSender(myAgent.getAID());
 				message.addReceiver(winner);
-				message.setContent("Ronda ganada de " + book + ". Precio: " + price + " euros");
-				message.setConversationId(book);
+				message.setContent("Ronda ganada de " + book + ". Precio: "
+						+ book.getPrice() + " euros");
+				message.setConversationId(book.getTitle());
 				myAgent.send(message);
-				
+
 				message = new ACLMessage(ACLMessage.REJECT_PROPOSAL);
 				message.setSender(myAgent.getAID());
 				message.setContent("No has ganado esta ronda de " + book);
-				message.setConversationId(book);
+				message.setConversationId(book.getTitle());
 				for (AID buyer : buyerAgents) {
-					if (!buyer.equals(winner)){
+					if (!buyer.equals(winner)) {
 						message.addReceiver(buyer);
 					}
 				}
 				myAgent.send(message);
-				
+
 				boolean ended = false;
-				
+
 				if (interestedCount == 0) {
-					//No hay interesados en la ronda actual
+					// No hay interesados en la ronda actual
 					if (winner != null) {
-						System.out.println("Subasta de " + book + " ganada por " + winner.getLocalName() + " por haber ganado la ronda anterior");
+						System.out.println("Subasta de " + book
+								+ " ganada por " + winner.getLocalName()
+								+ " por haber ganado la ronda anterior");
 						ended = true;
 					}
-				}
-				else if (interestedCount == 1) {
-					//Sólo hay un interesado en la ronda actual
-					System.out.println("Subasta de " + book + " ganada por " + winner.getLocalName() + " por ser el único interesado en la ronda actual");
+				} else if (interestedCount == 1) {
+					// Sólo hay un interesado en la ronda actual
+					System.out
+							.println("Subasta de "
+									+ book
+									+ " ganada por "
+									+ winner.getLocalName()
+									+ " por ser el único interesado en la ronda actual");
 					ended = true;
+				} else {
+					// hay más interesados en la ronda actual
+					book.updatePrice();
+					subastador.updatePrice(book);
 				}
-				else {
-					//hay más interesados en la ronda actual
-					price += increase;
-				}
-				
+
 				if (ended == true) {
-					/*Informar a todos los participantes que no han ganado la subasta*/
+					/*
+					 * Informar a todos los participantes que no han ganado la
+					 * subasta
+					 */
 					message = new ACLMessage(ACLMessage.INFORM);
 					message.setSender(myAgent.getAID());
-					message.setConversationId(book);
-					message.setContent("Subasta de " + book + " finalizada. El libro se vende por " + String.valueOf(price));
+					message.setConversationId(book.getTitle());
+					message.setContent("Subasta de " + book
+							+ " finalizada. El libro se vende por "
+							+ String.valueOf(book.getPrice()));
 					for (AID buyer : buyerAgents) {
 						if (!buyer.equals(winner)) {
 							message.addReceiver(buyer);
 						}
 					}
 					myAgent.send(message);
-					
+
 					/***************************************************************/
-					
-					/*Solicitar pago al ganador de la subasta*/
+
+					/* Solicitar pago al ganador de la subasta */
 					message = new ACLMessage(ACLMessage.REQUEST);
 					message.setSender(myAgent.getAID());
-					message.setConversationId(book);
-					message.setContent("Has ganado la subasta de " + book + " por " + String.valueOf(price) + " por favor, realiza el pago");
+					message.setConversationId(book.getTitle());
+					message.setContent("Has ganado la subasta de " + book
+							+ " por " + String.valueOf(book.getPrice())
+							+ " por favor, realiza el pago");
 					message.addReceiver(winner);
 					myAgent.send(message);
 					/******************************************/
+
+					subastador.updateWinner(book, winner.getLocalName());
 					this.stop();
 				}
-			}	
+			}
 		} catch (FIPAException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 	}
 }
