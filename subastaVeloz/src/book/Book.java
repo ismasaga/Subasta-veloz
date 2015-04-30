a611d7f3b48214c60989a3a469644e2192abea52
Cristina <Cristina@Lenovo-z510>
2015-04-30 17:24:28 +0200

    Book: Add new attributes

--- subastaVeloz/src/book/Book.java
+++ subastaVeloz/src/book/Book.java
@@ -3,26 +3,65 @@ package book;
 public class Book {
 	private String title;
 	private float price;
-	
-	public Book (String title, float price) {
+	private float increase;
+	private String winner;
+
+	public Book(String title, float price) {
 		this.title = title;
 		this.price = price;
 	}
-	
-	public Book (String title) {
+
+	public Book(String title, float price, float increase) {
+		this.title = title;
+		this.price = price;
+		this.increase = increase;
+	}
+
+	public Book(String title, float price, float increase, String winner) {
+		this.title = title;
+		this.price = price;
+		this.increase = increase;
+		this.winner = winner;
+	}
+
+	public Book(String title) {
 		this.title = title;
 	}
+
 	public Float getPrice() {
 		return price;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
+	public float getIncrease() {
+		return increase;
+	}
+
+	public String getWinner() {
+		return winner;
+	}
+
 	public void setPrice(Float price) {
 		this.price = price;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public void setIncrease(float increase) {
+		this.increase = increase;
+	}
+
+	public void setWinner(String winner) {
+		this.winner = winner;
+	}
+
+	public void updatePrice() {
+		price += increase;
 	}
 
 	@Override
@@ -49,6 +88,5 @@ public class Book {
 			return false;
 		return true;
 	}
-	
-	
+
 }
