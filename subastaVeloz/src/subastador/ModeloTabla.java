5d89061e4e4b36c31acf73c47334839020b437d9
Cristina <Cristina@Lenovo-z510>
2015-04-30 17:26:15 +0200

    ModeloTabla: Display winner name

---
+++ subastaVeloz/src/subastador/ModeloTabla.java
@@ -0,0 +1,80 @@
+package subastador;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import book.Book;
+
+public class ModeloTabla extends AbstractTableModel {
+
+	public ArrayList<Book> books;
+
+	public ModeloTabla(ArrayList<Book> books) {
+		this.books = books;
+		fireTableDataChanged();
+	}
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		return super.getColumnClass(columnIndex);
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 3;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		String tmp = "";
+		switch (column) {
+		case 0:
+			tmp = "Libro";
+			break;
+		case 1:
+			tmp = "Estado";
+			break;
+		case 2:
+			tmp = "Ganador";
+			break;
+		}
+		return tmp;
+	}
+
+	@Override
+	public int getRowCount() {
+		return books.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+			return books.get(rowIndex).getTitle();
+		case 1:
+			return books.get(rowIndex).getPrice();
+		case 2:
+			return books.get(rowIndex).getWinner();
+		}
+		return "default";
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return super.isCellEditable(rowIndex, columnIndex);
+	}
+
+	public void changeStatus(Book book) {
+		int i = 0;
+		int temp = 0;
+		for (Book b : books) {
+			if (b.equals(book)) {
+				temp = i;
+			}
+			i++;
+		}
+		books.set(temp, book);
+		fireTableDataChanged();
+	}
+}
